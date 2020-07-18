78413a099c9670dd0d87926111e49841bca60b48
w3cj <cj@null.computer>
2020-07-18 17:23:42 -0600

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Build a Simple Web Page that Listens for Twitch Chat
+
+* [x] Setup
+* [x] Listen for messages in channel
+* [x] Listen for !start-count
+  * Count all messages from unique users that have the number 1 in them
+* [x] Display total count on webpage
+* [x] Display name of all users that sent a message
+* [x] Listen for !stop-count
+* [x] Use text to speech to say the count out loud
---
+++ app.js
@@ -0,0 +1,44 @@
+const countElement = document.querySelector('#count');
+const usersElement = document.querySelector('#users');
+const statusElement = document.querySelector('#status');
+
+const params = new URLSearchParams(window.location.search);
+const channel = params.get('channel') || 'codinggarden';
+const client = new tmi.Client({
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: [channel],
+});
+
+client.connect().then(() => {
+  statusElement.textContent = `Listening for messages in ${channel}...`;
+});
+
+let listeningForCount = false;
+let users = {};
+
+client.on('message', (wat, tags, message, self) => {
+  if (self) return;
+  const { username } = tags;
+  if (username.toLowerCase() === channel.toLowerCase()) {
+    if (message === '!start-count') {
+      listeningForCount = true;
+    } else if (message === '!end-count') {
+      listeningForCount = false;
+      // say count out loud.
+      const sayCount = new SpeechSynthesisUtterance(Object.keys(users).length);
+      window.speechSynthesis.speak(sayCount);
+    } else if (message === '!clear-count') {
+      countElement.textContent = 'Waiting for count...';
+      usersElement.textContent = '';
+      users = {};
+    }
+  } else if (listeningForCount && message === '1') {
+    users[tags.username] = true;
+    // display current count page.
+    countElement.textContent = Object.keys(users).length;
+    usersElement.textContent = Object.keys(users).join(', ');
+  }
+});
