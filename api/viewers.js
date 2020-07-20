dc2bb468813ef496d9e667282968fcab022e1b09
S1ant <trb.9900@gmail.com>
2020-07-20 12:02:29 -0500

    Test Getting User_ID

--- api/viewers.js
+++ api/viewers.js
@@ -1,4 +1,16 @@
 module.exports = (req, res) => {
+    const clientid = '91rougkc35nq63ygvxrix9kxzno4y9';
     const { channel = 'codinggarden' } = req.query;
-    res.status(200).send(`channel: ${channel}`);
+    const headers = {
+        'Client-ID': '91rougkc35nq63ygvxrix9kxzno4y9',
+        'Accept': 'application/vnd.twitchtv.v5+json'
+    }
+    const user_id = await fetch('https://api.twitch.tv/kraken/users', {
+        method: 'GET',
+        headers: headers
+    }).then(res => {
+        return res.json().then(data => {
+            return data.users['_id'];
+        });
+    });
 }
